a6a9d9385e0c6f133ed5ef6733641b35ad8aa92d
NotBlasto <justin.stewart2222@hotmail.com>
2023-04-03 18:14:50 -0700

    First Commit
    Committing all files

---
+++ PHP Scripts/index.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<title> Home Page </title>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'/>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'>
+</head>
+<body>
+<div class='container text-center'>
+	<h1> Home Page </h1>
+</div>
+
+<?php require 'master.php';?>
+	<p>Welcome to the Home Page</p>
+<?php require 'footer.php';?>
+</body>
+</html>
